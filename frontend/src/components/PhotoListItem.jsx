368164a16c709bf0262091fd06c6131f52197fee
Jaidenpearson <[jaidensrpearson@gmail.com]>
2025-02-05 21:26:56 -0800

    inital commit with photolistitems updated to take props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({sampleDataForPhotoListItem}) => {
+  
+  return (
+    <>
+    <div>
+      <img src={sampleDataForPhotoListItem.imageSource} alt="" />
+      <div>
+        <img src={sampleDataForPhotoListItem.profile} alt="" />
+        <div>
+          <p>{sampleDataForPhotoListItem.username}</p>
+          <p>{sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}</p>
+        </div>
+      </div>
+    </div>
+    </>
+  )
 };
 
 export default PhotoListItem;
